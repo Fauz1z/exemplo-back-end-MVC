d8f633acae1093e47ef1b6ec0a309ec728223020
Fauzi <xxfauzixx@gmail.com>
2022-10-21 20:17:28 -0300

    Add project files.

---
+++ exemplo back-end MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exemplo_back_end_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
